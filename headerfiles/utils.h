280622e2f186cbb2cb74bfdd5b47c017f7e64587
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2024-03-26 16:40:09 +0100

    add some comments

--- headerfiles/utils.h
+++ headerfiles/utils.h
@@ -4,9 +4,11 @@
 #include<string.h>
 #include<stdlib.h>
 
+// return type when splitting a string
 struct SplitString {
   char** strings;
   int len;
 };
 
+// split a string by a given char
 struct SplitString split_by(const char* string, char ch);
